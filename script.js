742bbbe9a1f36ecf64f45ae7523c0c6e033cceba
Aayush Kunwar <kunwar_aayush@live.com>
2024-05-27 09:20:47 +0545

    added length and redo

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 const inputText = document.querySelector("#userInput");
 const textSource = document.querySelector(".source");
 const timee = document.querySelector(".timer");
+const wpss = document.querySelector(".wps");
 // const text = "Yesterday, you said tommorow. Just do it.";
-const text = "foo bar";
+// let text = "foo bar baz buzz eggs omelet";
+let selectedLength = 15;
+let text = getWords(selectedLength);
+// console.log(text);
 
 inputText.value = "";
 
 let sourceIndex = 0; // this is for the word
 let cursorIndex = 0;
-const textWords = text.split(" ");
-const count = textWords.length;
+let textWords = text.split(" ");
+let count = textWords.length;
 // states = notStarted , playing, end
 let gameState = "notStarted";
 let startTime = undefined;
@@ -18,6 +22,7 @@ let timerStarted = false;
 // textSource.innerHTML = "Yesterday, you said tommorow. Just do it.";
 function renderText() {
 	textSource.innerHTML = "";
+	// ch now means word
 	text.split(" ").forEach((ch, index) => {
 		const temp = document.createElement("div");
 		temp.classList.add("inline");
@@ -34,6 +39,27 @@ function renderText() {
 
 renderText();
 inputText.addEventListener("input", handleUserInput);
+
+function resetEverything() {
+	sourceIndex = 0; // this is for the word
+	cursorIndex = 0;
+	textWords = text.split(" ");
+	count = textWords.length;
+	// states = notStarted , playing, end
+	gameState = "notStarted";
+	startTime = undefined;
+	timerStarted = false;
+	text = getWords(selectedLength);
+	textWords = text.split(" ");
+	renderText();
+	inputText.value = "";
+	inputText.focus();
+}
+
+function setLength(len) {
+	selectedLength = len;
+	resetEverything();
+}
 
 function handleUserInput(e) {
 	startHandleTime();
@@ -69,7 +95,7 @@ function startHandleTime() {
 	if (!timerStarted) {
 		timerStarted = true;
 		startTime = new Date();
-		console.log("started at " + startTime);
+		// console.log("started at " + startTime);
 		handleTime();
 	}
 }
@@ -82,7 +108,8 @@ function handleTime() {
 		// console.log(endTime);
 		// console.log(timeTaken);
 		const wpm = Math.floor((count / timeTaken) * 60);
-		timee.innerHTML = `Time: ${timeTaken} wps: ${wpm}`;
+		timee.innerHTML = timeTaken;
+		wpss.innerHTML = wpm;
 		return;
 	}
 	setTimeout(() => {
